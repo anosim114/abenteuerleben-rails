fd8e7e2ae5ea66b35c9cf742c70f8d2dbeb36cf2
anosim114 <arnold_siemens@pm.me>
2023-08-28 19:30:31 +0200

    try to fix pipeline

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,6 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  config.hosts << "v3.abenteuerleben-ev.de"
 end
