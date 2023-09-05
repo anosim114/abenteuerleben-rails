f829da9d5bf82ee343834f37367f2e413056a425
anosim114 <arnold_siemens@pm.me>
2023-09-05 21:29:55 +0200

    remove not null for postgresql on prod

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.where(["start_date >= ? OR end_date NOT NULL AND end_date > ?", Date.today, Date.today]).order('start_date ASC')
+    @events = Event.where(["start_date >= ? OR end_date >= ?", Date.today, Date.today]).order('start_date ASC')
     @message = Message.new
   end
 end
