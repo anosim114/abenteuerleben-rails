4fd705e43587cee1d8c5155fa0673744f8a59f3c
anosim114 <arnold_siemens@pm.me>
2023-09-15 16:59:02 +0200

    reenable pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,21 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   resources :events
   resources :pages
+
   get 'admin/dashboard'
+
   get '/login', to: 'session#login'
   post '/login', to: 'session#auth'
   get '/logout', to: 'session#logout'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   get "/downloads", to: 'home#downloads'
   get '/spenden', to: 'home#spenden'
 
   resources :messages
-  scope '/admin' do
-    
-  end
-
-  # get '/login'
-  # get '/logout'
-  # get '/impressum'
-  # get '/datenschutz'
-  # get '/quellen'
 
   # namespace :admin do
-  #   resource :messages
-  #   resource :events
   #   resource :helpers
   #   resource :participants
   # end
