0efe9983af6ea5d06a9a3933e9406dbef44aa97d
anosim114 <arnold_siemens@pm.me>
2023-12-06 23:09:06 +0100

    add notes about the controller methods

--- app/controllers/child_registrations_controller.rb
+++ app/controllers/child_registrations_controller.rb
@@ -3,36 +3,51 @@ class ChildRegistrationsController < ApplicationController
   before_action :set_parent, only: %i[ new_parent_base_info new ]
 
   def index
+    # show camp info and the registration button
   end
 
   def new_name
+    # add the parent name
   end
 
   def new_address
+    # add the parent address
   end
 
   def new_contact
+    # add the parent contact info
   end
 
   def new_member
+    # ask whether the parent in in the local church
   end
 
   def new_optional_church
+    # ask what church the parent is in, if not the local
   end
 
   def new_child_count
+    # ask how many children should be registered
   end
 
   def new_child_name
+    # as child n_th name
   end
 
   def new_child_camp
+    # ask what camp the child should participate in
   end
 
   def new_child_notes
+    # as for child notes (medical, general notes)
   end
 
   def acknowlege
+    # show an overview before the final registering
+  end
+
+  def create
+    # create with all given information
   end
 
   private
@@ -48,7 +63,7 @@ class ChildRegistrationsController < ApplicationController
     params.require(:parent).permit(
       :surname, :forename, :telephone, :email,
       :street, :house, :post, :city,
-      :member,
+      :member, :church
       child_attributes: [:id, :camp_id, :surname, :forename, :birthday, :medical_condition, :notes])
   end
 end
