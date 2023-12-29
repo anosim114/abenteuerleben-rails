30c11f3a9bc6a7d469dfbb1b6b93af2587844763
- <44647308+anosim114@users.noreply.github.com>
2023-12-29 23:16:40 +0100

    Add validations for camp model

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -3,6 +3,14 @@ class Camp < ApplicationRecord
   has_many :registrations, dependent: :destroy
   has_many :children, dependent: :destroy
 
+  validates :date_start, presence: true
+  validates :date_end, presence: true
+  validates :participants_year_start, presence: true
+  validates :participants_year_end, presence: true
+  validates :max_participant_count, presence: true
+  validates :price, presence: true
+  validates :campyear, presence: true
+
   def open_count
     max_participant_count - children.length
   end
