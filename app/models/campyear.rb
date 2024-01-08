7cd4b42d27512ce9b7c96790da2edb8cdf180e10
- <44647308+anosim114@users.noreply.github.com>
2024-01-08 21:46:10 +0100

    Fix campyear validation

--- app/models/campyear.rb
+++ app/models/campyear.rb
@@ -1,10 +1,17 @@
 class Campyear < ApplicationRecord
   has_many :camps, dependent: :destroy
 
+  validates :participants_register_start, presence: true
+  validates :members_only_start, presence: true
+  validates :participants_register_end, presence: true
+
   validate :participant_member_date_earlier_all_date
 
   def participant_member_date_earlier_all_date
-    return if participants_register_start >= members_only_start
+    if !(participants_register_start.nil? || members_only_start.nil?) &&
+       participants_register_start >= members_only_start
+      return
+    end
 
     errors.add :members_only_date, 'muss gleich oder früher als der Start aller sein'
   end
