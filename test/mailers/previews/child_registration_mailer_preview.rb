15191f86bd1b57222947bd35e3496e8c7b750942
anosim114 <44647308+anosim114@users.noreply.github.com>
2024-01-13 10:05:59 +0100

    Extend child registration checkboxes (#143)
    * Add datenschutz, media-confirm and rules for cancelling checkboxes to child registration
    
    * Extend parent mail with links to content they agreed to

--- test/mailers/previews/child_registration_mailer_preview.rb
+++ test/mailers/previews/child_registration_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/child_registration_mailer
 class ChildRegistrationMailerPreview < ActionMailer::Preview
+  def registered_mail
+    parent = Parent::Parent.first!
 
+    ChildRegistrationMailer.with(parent:).registered_mail
+  end
 end
