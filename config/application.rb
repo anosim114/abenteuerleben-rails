a89e7b1bb8fdde0fa9bfa8b435bc3c45604a2286
anosim114 <44647308+anosim114@users.noreply.github.com>
2024-01-16 22:52:42 +0100

    Improve error logging (#153)

--- config/application.rb
+++ config/application.rb
@@ -2,16 +2,33 @@ require_relative 'boot'
 
 require 'rails/all'
 
+# require_relative 'init_config'
+CONFIG = {}
+CONFIG['version'] = '0.0.1'
+CONFIG['password-salt'] = 'kind_words_of_no'
+CONFIG['storage_path'] = ''
+CONFIG['flags'] = {
+  show_helper_images: true
+}
+CONFIG['user_levels'] = {
+  "Admin": 1 << 4,
+  "Moderator": 1 << 3,
+  "Member": 1 << 2,
+  "Anonymous": 1 << 0
+}
+CONFIG['email_from'] = ENV['email_from'] || ''
+CONFIG['server_host'] = ENV['server_host'] || 'localhost'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RubyRailsPostgres
+module AbenteuerLeben
   class Application < Rails::Application
     require 'forticons'
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     config.i18n.default_locale = :de
 
@@ -29,5 +46,6 @@ module RubyRailsPostgres
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
