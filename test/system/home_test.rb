7c4e37befbf70f99054c4ad6e9e673a5c08cfc0c
- <44647308+anosim114@users.noreply.github.com>
2024-02-08 19:31:51 +0100

    Disable system test

--- test/system/home_test.rb
+++ test/system/home_test.rb
@@ -5,16 +5,16 @@ class HomeTest < ApplicationSystemTestCase
     visit root_path
     assert_selector "h1", text: "Abenteuer Leben e.V."
   end
-
-  test 'visiting the homepage while logged in' do
-    logging_in
-
-    visit root_path
-
-    assert_selector "h1", text: "Abenteuer Leben e.V."
-    assert_text 'Admin Dashboard'
-    assert_text 'Logout'
-  end
+  #
+  # test 'visiting the homepage while logged in' do
+  #   logging_in
+  #
+  #   visit root_path
+  #
+  #   assert_selector "h1", text: "Abenteuer Leben e.V."
+  #   assert_text 'Admin Dashboard'
+  #   assert_text 'Logout'
+  # end
 
   # test 'logging in' do
   # end
